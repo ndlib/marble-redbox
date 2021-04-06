86247fffe3800b61aa3c102283dcba97aef68772
Jon Hartzler <jon.hartzler@gmail.com>
2021-04-06 14:17:15 -0400

    update redbox's keys

--- scripts/setup-development.sh
+++ scripts/setup-development.sh
@@ -1,4 +1,8 @@
-if [ ! -f .env.development ]; then
-  cp .env.test .env.development
-  echo "Created .env.development and double check your keys."
-fi
+cp .env.development-example .env.development
+
+GRAPHQL_API_KEY_BASE_PATH='/all/stacks/sm-test-maintain-metadata/'
+# add the app sync keys to the env
+node ./scripts/codebuild/setupEnv.js ${GRAPHQL_API_KEY_BASE_PATH} >> '.env.development' --unhandled-rejections=strict
+
+node ./scripts/codebuild/setupEnv.js ${GRAPHQL_API_KEY_BASE_PATH}
+echo "Reset .env.development and double check your keys."
